61fd7df94c29feef22685efcbc7daf34902fc7d6
ghesshu <hughestotimee@gmail.com>
2023-06-10 08:27:01 +0000

    update

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="pb-4">
       <div className="max-w-6xl xl:max-w-6xl mx-auto divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
-        <ul className="Footer_nav__2rFid text-sm font-medium  sm:pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10">
+        {/* <ul className="Footer_nav__2rFid text-sm font-medium  sm:pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10">
           <li className="space-y-5 row-span-2">
             <h2 className="text-sm tracking-wide text-gray-900 uppercase font-bold">
               Company
@@ -16,6 +16,7 @@ export default function Footer() {
                   Jobs
                 </a>
               </li>
+
               <li>
                 <a
                   className="hover:text-gray-900 transition-colors duration-200 text-2xl"
@@ -143,7 +144,7 @@ export default function Footer() {
               </li>
             </ul>
           </li>
-        </ul>
+        </ul> */}
         <div className="flex flex-col-reverse justify-between pt-5 pb-4 border-t lg:flex-row bg-top border-black">
           <ul className="flex flex-col space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
             <li>
@@ -183,7 +184,7 @@ export default function Footer() {
                 href="/"
                 className="text-md text-black transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold"
               >
-                Partners
+                Blog
               </a>
             </li>
           </ul>
@@ -192,7 +193,7 @@ export default function Footer() {
               href="/"
               className="text-md text-black transition-colors duration-300 hover:text-deep-purple-accent-400 font-semibold tracking-tight"
             >
-              © 2021 Company Inc.
+              © 2023 Company Inc.
             </a>
           </ul>
         </div>
--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
             className="text-lg font-semibold rounded-lg tracking-widest focus:outline-none focus:shadow-outline"
           >
             <h1 className="text-4xl Avenir tracking-tighter text-gray-900 md:text-4x1 lg:text-3xl">
-              NINE4
+              DOB
             </h1>
           </a>
           <button
@@ -47,14 +47,14 @@ export default function Header() {
         >
           <nav className="flex-col flex-grow ">
             <ul className="flex flex-grow justify-end flex-wrap items-center">
-              <li>
+              {/* <li>
                 <a
                   href="/"
                   className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
                 >
                   Careers
                 </a>
-              </li>
+              </li> */}
               <li>
                 <a
                   href="/"
@@ -68,7 +68,7 @@ export default function Header() {
                   className="inline-flex items-center px-4 py-2 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg text-md md:mt-0 md:ml-4 bg-gray-900"
                   href="/"
                 >
-                  <span className="justify-center">Download</span>
+                  <span className="justify-center">Contact Us</span>
                   <svg
                     className="w-3 h-3 fill-current text-gray-400 flex ml-2 -mr-1"
                     viewBox="0 0 12 12"
